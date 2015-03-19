b5707bbde5e530a9a6fd9ddf242940f810edf643
trfergusASU <trfergus@asu.edu>
2015-03-19 13:55:47 -0700

    Update to actually compile
    compile errors fixed

--- q.h
+++ q.h
@@ -6,18 +6,94 @@
 #include <string.h>
 #include "TCB.h"
 
-struct q {
-	struct TCB_t *header;
-};
+
 
 struct queue {
-    struct q* head;
-    struct q* elements;
+    struct TCB_t* head;
+    struct TCB_t* elements;
 };
 
 void debug(const char * format, ...) ;
 struct TCB_t* newItem();
-struct queue* initQ(struct q* head);
-void addQ(struct q* head,struct TCB_t* index);
-struct TCB_t* delQ(struct q* head);
-void rotateQ(struct q* head);
+struct queue* initQ(struct TCB_t* head);
+void addQ(struct TCB_t* head,struct TCB_t* index);
+struct TCB_t* delQ(struct TCB_t* head);
+void rotateQ(struct TCB_t* head);
+
+
+void debug(const char * format, ...) {
+    int DEBUG = 1;
+//    if (DEBUG) {
+//        printf("\nDEBUG:%s", string);
+//    }
+    va_list args;
+    if (DEBUG)
+    {
+        va_start (args, format);
+        vfprintf (stdout, format, args);
+        va_end (args);
+    }
+}
+
+struct TCB_t* newItem()
+{
+    struct TCB_t* item = (struct TCB_t*) malloc(sizeof(struct TCB_t));
+    item->prev = NULL;
+    item->next = NULL;
+    //item->context = -1;
+    return item;
+    
+}
+
+struct queue* initQ(struct TCB_t* head) {//Creates an empty queue pointed to by head var.
+    debug("init");
+    struct TCB_t* hed = head;
+    debug("malloc queue");
+    struct queue* newQueue = (struct queue*) malloc(sizeof(struct queue));
+    debug("after queue");
+    newQueue->elements = hed;
+    newQueue->head = hed;
+    debug("return queue");
+    return newQueue;
+}
+
+void addQ(struct TCB_t* head, struct TCB_t* item) { //Adds a queue item
+    struct TCB_t* it = item;
+    struct TCB_t* he = head;
+   	printf("added new item");
+   // printf("\nAdd new Item to queue:\n%d", he->payload);
+    while (he->next != NULL) {
+        he = he->next;
+       // printf("\n%d", he->payload);
+    }
+    he->next = newItem();
+    he->next = it;
+    //printf("\n%d", he->next->payload);
+	printf("Payload");
+}
+
+struct TCB_t* delQ(struct TCB_t* head) {
+    debug("\nDelete Head");
+    struct TCB_t* temp = head;
+    head = head->next;
+    head->prev = NULL;
+    //debug("%d", head->payload);
+    return temp;
+}
+
+void rotateQ(struct TCB_t* head) {
+    head = head->next;
+    head->prev = NULL;
+}
+
+void printElements (struct queue* que) {
+    printf("\nPrint Elements");
+    struct queue* temp = que;
+    if (temp->elements != NULL) {
+        while (temp->elements->next != NULL) {
+            temp->elements = temp->elements->next;
+			printf("item found");
+            //printf("\n%d", temp->elements->payload);
+        }
+    }
+}
--- thread_test.c
+++ thread_test.c
@@ -5,7 +5,87 @@
 #include "threads.h"
 
 
-int globalRuns = 0; 
+int globalRuns = 0; // Students- Tim Ferguson and Josh Ransom
+
+
+
+#include "threads.h"
+
+
+int globalRuns = 0; // Increment after each run
+struct queue *RunQ;
+
+
+void f1(void) {
+
+	int local = 0;
+
+	if (DEBUG) printf("THREAD 1: STARTED!!!\n");
+
+	for (;;) {
+		printf("THREAD 1:\tGLOBAL: %d\tLOCAL: %d\n", globalRuns, local);	
+		globalRuns++;
+		local++;
+		sleep(1);
+		yield();
+	}
+
+	return;
+}
+
+
+void f2(void) {
+	// Simulate Power of 2
+	int local = 2;
+
+	if (DEBUG) printf("THREAD 2: STARTED!!!\n");
+
+	for (;;) {
+		printf("THREAD 2:\tGLOBAL: %d\tLOCAL: %d\n", globalRuns, local);	
+		globalRuns++;
+		local *= 2;
+		sleep(1);
+		yield();
+	}
+	
+	return;
+}
+
+//-----------//
+// f3 Method //
+//-----------//
+void f3(void) {
+	// Multiples of 3
+	int local = 3;
+
+	if (DEBUG) printf("THREAD 3: STARTED!!!\n");
+
+	for (;;) {
+		printf("THREAD 3:\tGLOBAL: %d\tLOCAL: %d\n", globalRuns, local);
+		globalRuns++;
+		local += 3;
+		sleep(1);
+		yield();
+	}
+
+	return;
+}
+
+//-------------//
+// main Method //
+//-------------//
+int main() {
+	RunQ = (struct TCB_t*) malloc(sizeof(struct TCB_t));
+
+	initQueue(RunQ->head);
+
+	startThread(f1);
+	startThread(f2);
+	startThread(f3);
+	run();
+
+	return 0;
+}
 TCB_t *RunQ;
 
 
