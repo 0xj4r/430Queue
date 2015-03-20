cef8a4bcd0c6c6fdf02bd14825ab4d7fd371ec87
JRan <joshr@jran-Ubuntu.(none)>
2015-03-19 17:18:30 -0700

    Wrote dem codez

--- TCB.h
+++ TCB.h
@@ -12,7 +12,7 @@ void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
 {
     memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
     getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
-    tcb->context.uc_stack.ss_sp = stackP;
-    tcb->context.uc_stack.ss_size = (size_t) stack_size;
+    tcb->context.uc_stack.ss_sp = malloc(8192);
+    tcb->context.uc_stack.ss_size = 8192;
     makecontext(&tcb->context, function, 0);// context is now cooked
 }
