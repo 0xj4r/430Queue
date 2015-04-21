eddb8e810acbae3836d23b275d6e04946c26141c
Josh Ransom <josh.bh.ransom@gmail.com>
2015-04-20 18:58:18 -0700

    Added code from the notes and a few other bits to project file

--- proj-4.c
+++ proj-4.c
@@ -5,66 +5,87 @@
 
 int globalRuns = 0;
 struct queue *RunQ;//global Q
-struct Semaphore* obama;
+struct Semaphore* mutex;
+struct Semaphore* readerSem;
+struct Semaphore* writerSem;
+int wwc = 0;
+int rwc = 0;
+int rc =0;
+int wc = 0;
 
-
-void f1(void) {
-    int localVar = 1;
+void reader(int num) {
     while (1) {
-        P(obama);
-        // Critical Section
-        printf("Thread 1:\tGlobal: %d\tLocal: %d\n", globalRuns, localVar);
-        globalRuns ++;
-        localVar *=2;
-        V(obama);
+        P(mutex);
+        if (wwc > 0 || wc > 0) {
+            wwc++;
+            V(mutex);
+            P(readerSem);
+            P(mutex);
+            rwc--;
+        }
+        rc++;
+        V(mutex);
+        P(mutex);
+        printf("READER ");
+        rc--;
+        if (rc ==0 && wwc > 0) {
+            V(writerSem);
+        }
+        return;
+
     }
-    return;
 }
 
-void f2(void) {
-    int localVar = 1;
-    while (1) {
-        P(obama);
-        // Critical Section
-        printf("Thread 2:\tGlobal: %d\tLocal: %d\n", globalRuns, localVar);
-        globalRuns ++;
-        localVar *=3;
-        V(obama);
+void writer(int num) {
+        while (1) {
+        P(mutex);
+        if(rc > 0 || wc > 0 || rwc > 0 || wwc > 0) {
+            wwc ++;
+            V(mutex);
+            P(wsem);
+            P(mutex);
+            wwc--;
+        }
+        wc ++;
+        V(mutex);
+        P(mutex);
+        printf("WRITER");
+        wc--;
+        if (rwc > 0 ) {
+            for (int i =0; i <= rwc; i++) { V(readerSem); }
+        }
+        else if (wwc > 0) { V(writerSem) }
+        V(mutex);
+        return;
     }
-    return;
 }
 
 
-void f3(void) {
-    int localVar = 1;
-    printf("Thread 3: has begun! - This thread multiplies the local variable by the global each time\n");
-    
-    while (1) {
-        printf("Thread 3:\tGlobal: %d\tLocal: %d\n", globalRuns, localVar);
-        localVar = localVar*globalRuns;
-        globalRuns++;
-        sleep(1);
-        yield();
-        //printf("YIELD FUNK 3\n");
-    }
-    return;
-}
 
+
+void startSemaphore(struct Semaphore* sem, int initial) {
+    sem = (struct Semaphore*) malloc(sizeof(struct Semaphore));
+    sem->semQ = initQ(sem->head);
+    InitSem(sem, initial);
+}
 
 int main() {
-    obama = (struct Semaphore*) malloc(sizeof(struct Semaphore));
-    obama->semQ = initQ(obama->head);
-    InitSem(obama, 0);
-    
+//    obama = (struct Semaphore*) malloc(sizeof(struct Semaphore));
+//    obama->semQ = initQ(obama->head);
+//    InitSem(obama, 0);
+    startSemaphore(readerSem, 0);
+    startSemaphore(mutex, 1);
+    startSemaphore(writerSem, 1);
     RunQ = (struct queue*) malloc(sizeof(struct queue)); //aloc Q
-    
     RunQ = initQ(RunQ->head);//get the party rolling
+    printf("Starting Readers and Writers");
+    start_thread(reader(1));
+    start_thread(writer(1));
+//    start_thread(reader(2));
+//    start_thread(writer(2));
+//    start_thread(reader(3));
+//    start_thread(writer(3));
     
-    printf("Starting Threads - Increment global by 1 for each context switch");
-    
-    start_thread(f1);//starting
-    start_thread(f2);//some
-//    start_thread(f3);//threads
     run();//RUN EM
     
     return 0;
